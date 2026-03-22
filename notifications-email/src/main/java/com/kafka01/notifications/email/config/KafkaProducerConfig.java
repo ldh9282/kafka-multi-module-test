850b79fe05327fd7d47cc8d38906800675f17f99
ldh9282 <ldh9282@naver.com>
2026-03-22 23:37:13 +0900

    feat(notifications-email): Add Kafka producer configuration

---
+++ notifications-email/src/main/java/com/kafka01/notifications/email/config/KafkaProducerConfig.java
@@ -0,0 +1,43 @@
+package com.kafka01.notifications.email.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+    private final Environment env;
+
+    public KafkaProducerConfig(Environment env) {
+        this.env = env;
+    }
+
+    private Map<String, Object> producerConfigs() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("spring.kafka.consumer.bootstrap-servers"));
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+        return configs;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+}
